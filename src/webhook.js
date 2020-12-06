1d1bf4b211e5989f22ca3bf68520df9bed7d0e1f
k1w1 <chris@aha.io>
2020-12-05 16:47:34 -0800

    Working on JS code for GitHub webhooks

--- src/webhook.js
+++ src/webhook.js
@@ -1,6 +1,89 @@
-Aha.on("webhook", async (args) => {
+Aha.on("webhook", async ({ headers, payload }) => {
   console.log("start");
-  console.log(`Received webhook ${args["action"]}`);
-  console.log(args);
+
+  const event = headers.HTTP_X_GITHUB_EVENT;
+
+  console.log(
+    `Received webhook ${headers.HTTP_X_GITHUB_EVENT} ${payload.action}`
+  );
+
+  switch (event) {
+    case "create":
+      await handleCreate(payload);
+      break;
+    case "push":
+      handlePush(payload);
+      break;
+  }
+
   console.log("done");
 });
+
+function handlePush(payload) {}
+
+async function handleCreate(payload) {
+  // We only care about branches.
+  if (payload.ref_type != "branch") {
+    return;
+  }
+
+  const branchName = payload.ref;
+  const ahaReference = extractReference(branchName);
+  if (ahaReference) {
+    // Link branch to Aha! record.
+    console.log(`Link to ${ahaReference.type}:${ahaReference.referenceNum}`);
+
+    fields = await graphFetch(
+      `{ feature(id: "${ahaReference.referenceNum}") { extensionFields } }`
+    );
+
+    console.log(fields);
+  }
+}
+
+function extractReference(name) {
+  let matches;
+
+  // Requirement
+  if ((matches = name.match(/[a-z]{1,10}-[0-9]+-[0-9+]/i))) {
+    return {
+      type: "Requirement",
+      referenceNum: matches[0],
+    };
+  }
+  // Epic
+  if ((matches = name.match(/[a-z]{1,10}-E-[0-9+]/i))) {
+    return {
+      type: "Epic",
+      referenceNum: matches[0],
+    };
+  }
+  // Feature
+  if ((matches = name.match(/[a-z]{1,10}-[0-9+]/i))) {
+    return {
+      type: "Feature",
+      referenceNum: matches[0],
+    };
+  }
+
+  return null;
+}
+
+async function graphFetch(query) {
+  const response = await fetch("http://reallybigaha.ngrok.io/graphql", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      query: query,
+    }),
+  });
+
+  if (response.status != 200) {
+    throw new Error(
+      `GraphQL fetch failed: ${response.status} ${
+        response.statusText
+      }: ${await response.text()}`
+    );
+  }
+  return await response.json();
+}
