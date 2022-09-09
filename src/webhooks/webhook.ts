ba6348563d6bb573bb181907be39dafba491bfa8
Percy Hanna <percy@aha.io>
2022-09-09 11:13:56 -0600

    Handle converted_to_draft event

--- src/webhooks/webhook.ts
+++ src/webhooks/webhook.ts
@@ -55,6 +55,8 @@ const findAutomationTrigger = (
       return payload.pull_request.merged ? "prMerged" : "prClosed";
     case "opened":
       return payload.pull_request.draft ? "draftPrOpened" : "prOpened";
+    case "converted_to_draft":
+      return "draftPrOpened";
     case "ready_for_review":
       return "draftReadyForReview";
     case "reopened":
