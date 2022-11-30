3681a11d14caa38ae87c882505176132c54ecc5c
Percy Hanna <percy@aha.io>
2022-11-30 13:19:48 -0700

    Update the empty state link as well

--- src/lib/extension.ts
+++ src/lib/extension.ts
@@ -1,1 +1,5 @@
 export const IDENTIFIER = "aha-develop.github";
+
+export const LEARN_MORE_URL =
+  "https://www.aha.io/support/develop/integrations/github/github-extension";
+export const ICON = aha.iconForExtensionIdentifier(IDENTIFIER);
